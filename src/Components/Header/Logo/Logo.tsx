53d2fe3d15c53dc53fee7e71bbd8c5550c144314
Alichatraei <avida.chatraee2001@yahoo.com>
2021-07-16 18:22:11 +0430

    Add custom type to the project for accept svg type in typescript

---
+++ src/Components/Header/Logo/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import GOTLogo from "../../../Assets/Images/GOT.svg";
+const Logo = () => {
+  return (
+    <>
+      <GOTLogo />
+    </>
+  );
+};
+
+export default Logo;
